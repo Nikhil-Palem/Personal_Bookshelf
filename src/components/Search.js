4ebf5040f0e1cbab34f38610a81003935138a690
Nikhil Palem <nikhlipalem93466@gmail.com>
2024-06-05 16:44:21 +0530

    error handling

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,11 +6,18 @@ import './Search.css'
 const Search = ({ addToBookshelf }) => {
   const [query, setQuery] = useState('');
   const [books, setBooks] = useState([]);
+const [Error, setError] = useState("");
 
   const searchBooks = async (e) => {
     e.preventDefault();
     const response = await axios.get(`https://openlibrary.org/search.json?q=${query}&limit=10&page=1`);
-    setBooks(response.data.docs);
+    
+    if(response.data.docs.length===0){
+      setError("no book's found... please retry");
+    }else{
+      setBooks(response.data.docs);
+      setError("");
+    }
     setQuery("")
   };
 
@@ -21,6 +28,9 @@ const Search = ({ addToBookshelf }) => {
         <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} placeholder="Search for books" />
         <button type="submit">Search</button>
       </form>
+
+      {Error&&<p style={{color:"red",fontSize:"13px",fontWeight:"bold",textAlign:"center"}}>{Error}</p> }
+
       <div className="book-results">
         {books.map((book, index) => (
           <BookCard key={index} book={book} addToBookshelf={addToBookshelf} />
